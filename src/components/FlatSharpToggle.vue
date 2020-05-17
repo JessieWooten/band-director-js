d922d4706e9309a768d400a2b1b2931ddb846f5a
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-17 01:10:57 -0500

    feature: Toggles flat/sharp note settings

---
+++ src/components/FlatSharpToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex toggle-flats">
+    <span :class="{'active':!$store.state.view.showFlats}">#</span>
+    <input type="checkbox" name="show-flats" v-model="$store.state.view.showFlats" />
+    <label for="show-flats" @click="$store.commit('toggleSharps')"></label>
+    <span :class="{'active': $store.state.view.showFlats}">♭</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
