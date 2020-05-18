f23b2661ac4951392dd24005cb1b2013047af54c
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-18 14:15:09 -0500

    refactor: foundListeners as object with ip as key

--- src/store/modules/session/index.js
+++ src/store/modules/session/index.js
@@ -1,118 +1,134 @@
 const SESSION = {
-    state:{
-        foundListeners:[],
-        soloList:[],
-        muteList:[],
-        isIncomingInvitation: false,
-        incomingInvitation: null,
-        sessionLeadIp:null,
-        isListener: false
+  state: {
+    foundListeners: {},
+    soloList: [],
+    muteList: [],
+    isIncomingInvitation: false,
+    incomingInvitation: null,
+    sessionLeadIp: null,
+    isListener: false,
+  },
+  mutations: {
+    addToListenerList(state, { name, ip }) {
+      let onList = state.foundListeners[ip];
+      if (!onList) {
+        state.foundListeners = {
+          ...state.foundListeners,
+          [ip]: {
+            name: decodeURIComponent(name),
+            ip,
+            invited: false,
+            accepted: false,
+          },
+        };
+      }
     },
-    mutations:{
-        addToListenerList(state, {name, ip}){
-            let onList = false;
-            for(var i=0; i<state.foundListeners.length;i++){
-                if(state.foundListeners[i].ip ===ip){onList = true; break;}
-            }
-            if(!onList){
-                state.foundListeners.push({name: decodeURIComponent(name), ip:ip, invited:false, accepted: false});
-            }
-        },
-        inviteToSession(state, index){
-            state.foundListeners[index].invited = true;
-        },
-        addToSession(state, index){
-            state.foundListeners[index].accepted = true;
-        },
-        removeFromSession(state, index){
-            state.foundListeners[index].accepted = false;
-            state.foundListeners[index].invited = false;
-        },
-        setInvitation(state, { isIncomingInvitation, incomingInvitation }){
-            state.incomingInvitation = incomingInvitation;
-            state.isIncomingInvitation = isIncomingInvitation;
-        },
-        setSessionLeadIp(state, ip){
-            state.sessionLeadIp = ip;
-        },
-        clearInvitation(state){
-            state.incomingInvitation = null;
-            state.isIncomingInvitation = false;
-        },
-        solo(state, ip){
-            if(state.soloList.indexOf(ip) === -1){
-                state.soloList.push(ip);
-            }else{
-                state.soloList.splice(state.soloList.indexOf(ip), 1);
-            }
-        },
-        mute(state, ip){
-            if(state.muteList.indexOf(ip) === -1){
-                state.muteList.push(ip);
-            }else{
-                state.muteList.splice(state.muteList.indexOf(ip), 1);
-            }
-        },
-        resetSoloMute(state){
-            state.soloList = [];
-            state.muteList = [];
-        },
-        setIsListener(state, isListener){
-            state.isListener = isListener;
-        }
+    inviteToSession(state, ip) {
+      state.foundListeners[ip].invited = true;
     },
-    actions:{
-        inviteToSession(context, index){
-            context.dispatch('sendInvite', context.state.foundListeners[index].ip);
-            context.commit('inviteToSession', index);
-        },
-        removeListenerFromSession(context, {ip}){
-            let index = -1;
-            for (var i =0; i < context.state.foundListeners.length; i++){
-                if(context.state.foundListeners[i].ip === ip){
-                    index = i;
-                    break;
-                }
-            }
-            if(index != -1){
-                context.dispatch('notify', `${decodeURIComponent(context.state.foundListeners[index].name)} has left your session.`)
-                context.commit('removeFromSession', index);
-            }
-        },
-        handleInvitationResponse(context, { ip, response }){
-            let index = -1;
-            for (var i =0; i < context.state.foundListeners.length; i++){
-                if(context.state.foundListeners[i].ip === ip){
-                    index = i;
-                    break;
-                }
-            }
-            if(parseInt(response) === 1){
-                context.commit('addToSession', index);
-                context.dispatch('notify', `${context.state.foundListeners[index].name} joined your session!`)
-            }else{
-                context.commit('removeFromSession', index);
-            }
-        },
-        clearSessionInfo(context){
-            context.commit('setReceivedMsgList', []);
-            context.commit('setNewMsg', {note:'', modifier:''});
-        }
+    addToSession(state, ip) {
+      state.foundListeners[ip].accepted = true;
     },
-    getters:{
-        sessionListeners(state){
-            return state.foundListeners.filter(listener => {return listener.invited === true && listener.accepted === true});
-        },
-        invitedListeners(state){
-            return state.foundListeners.filter(listener => {return listener.invited === true });
-        },
-        isSoloed(state){
-            return !!state.soloList.length;
-        },
-        isMuted(state){
-            return !!state.muteList.length;
-        }
-    }
-}
+    removeFromSession(state, ip) {
+      state.foundListeners[ip].accepted = false;
+      state.foundListeners[ip].invited = false;
+    },
+    setInvitation(state, { isIncomingInvitation, incomingInvitation }) {
+      state.incomingInvitation = incomingInvitation;
+      state.isIncomingInvitation = isIncomingInvitation;
+    },
+    setSessionLeadIp(state, ip) {
+      state.sessionLeadIp = ip;
+    },
+    clearInvitation(state) {
+      state.incomingInvitation = null;
+      state.isIncomingInvitation = false;
+    },
+    solo(state, ip) {
+      if (state.soloList.indexOf(ip) === -1) {
+        state.soloList.push(ip);
+      } else {
+        state.soloList.splice(state.soloList.indexOf(ip), 1);
+      }
+    },
+    mute(state, ip) {
+      if (state.muteList.indexOf(ip) === -1) {
+        state.muteList.push(ip);
+      } else {
+        state.muteList.splice(state.muteList.indexOf(ip), 1);
+      }
+    },
+    resetSoloMute(state) {
+      state.soloList = [];
+      state.muteList = [];
+    },
+    setIsListener(state, isListener) {
+      state.isListener = isListener;
+    },
+  },
+  actions: {
+    inviteToSession(context, ip) {
+      context.dispatch("sendInvite", context.state.foundListeners[ip].ip);
+      context.commit("inviteToSession", ip);
+    },
+    removeListenerFromSession(context, { ip }) {
+      if (!context.state.foundListeners[ip]) {
+        return context.dispatch(
+          "notify",
+          "ERROR: Cannot find user at the provided address."
+        );
+      }
+      context.dispatch(
+        "notify",
+        `${decodeURIComponent(
+          context.state.foundListeners[ip].name
+        )} has left your session.`
+      );
+      context.commit("removeFromSession", ip);
+    },
+    handleInvitationResponse(context, { ip, response }) {
+      if (!context.state.foundListeners[ip]) {
+        return context.dispatch(
+          "notify",
+          "ERROR: Cannot find user at the provided address."
+        );
+      }
+      if (parseInt(response) === 1) {
+        context.commit("addToSession", ip);
+        context.dispatch(
+          "notify",
+          `${context.state.foundListeners[ip].name} joined your session!`
+        );
+      } else {
+        context.commit("removeFromSession", ip);
+      }
+    },
+    clearSessionInfo(context) {
+      context.commit("setReceivedMsgList", []);
+      context.commit("setNewMsg", { note: "", modifier: "" });
+    },
+  },
+  getters: {
+    sessionListeners(state) {
+      return Object.values(state.foundListeners).filter((listener) => {
+        return listener.invited === true && listener.accepted === true;
+      });
+    },
+    invitedListeners(state) {
+      return Object.values(state.foundListeners).filter((listener) => {
+        return listener.invited === true;
+      });
+    },
+    foundListenersList(state) {
+      return Object.values(state.foundListeners);
+    },
+    isSoloed(state) {
+      return !!state.soloList.length;
+    },
+    isMuted(state) {
+      return !!state.muteList.length;
+    },
+  },
+};
 
 export default SESSION;
