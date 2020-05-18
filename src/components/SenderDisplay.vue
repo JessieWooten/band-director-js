075c50cffd73411e1c1092a64a52ae4ec2c18af2
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-17 23:38:46 -0500

    enhance: add FlatSharpToggle component

--- src/components/SenderDisplay.vue
+++ src/components/SenderDisplay.vue
@@ -1,38 +1,48 @@
 <template>
-    <section class="command-sender" :class="{'disabled': !$store.getters.sessionListeners.length === 0}" v-show="!$store.state.session.isListener">
-        <div class="flex row">
-            <Pad v-for="i in 7" :key="i" 
-                :display="$store.state.view.showFlats ? $store.state.view.scale[i]+'♭' : $store.state.view.scale[i-1]+'#'"  
-                :note="$store.state.view.showFlats ? $store.state.view.scale[i]+'♭' : $store.state.view.scale[i-1]+'#'" 
-                :index="i" :class="{'hidden':shouldBeHidden(i)}"/>
-        </div>
-        <div class="flex row">
-            <Pad v-for="i in 7" :key="i" :display="$store.state.view.scale[i-1]" :note="$store.state.view.scale[i-1]" :index="i-1" />
-        </div>
+  <section
+    class="command-sender"
+    :class="{'disabled': !$store.getters.sessionListeners.length === 0}"
+    v-show="!$store.state.session.isListener"
+  >
+    <div class="flex row">
+      <Pad
+        v-for="i in 7"
+        :key="i"
+        :display="$store.state.view.showFlats ? $store.state.view.scale[i]+'♭' : $store.state.view.scale[i-1]+'#'"
+        :note="$store.state.view.showFlats ? $store.state.view.scale[i]+'♭' : $store.state.view.scale[i-1]+'#'"
+        :index="i"
+        :class="{'hidden':shouldBeHidden(i)}"
+      />
+    </div>
+    <div class="flex row">
+      <Pad
+        v-for="i in 7"
+        :key="i"
+        :display="$store.state.view.scale[i-1]"
+        :note="$store.state.view.scale[i-1]"
+        :index="i-1"
+      />
+    </div>
 
-        <div class="flex toggle-flats">
-            <span :class="{'active':$store.state.view.showFlats}">♭</span>
-            <input type="checkbox" name="show-flats" v-model="!$store.state.view.showFlats">
-            <label for="show-flats" @click="$store.commit('toggleSharps')"></label>
-            <span :class="{'active':!$store.state.view.showFlats}">#</span>
-        </div> 
-    </section>
+    <FlatSharpToggle />
+  </section>
 </template>
 
 <script>
-import Pad from "@/components/Pad.vue"
+import FlatSharpToggle from "@/components/FlatSharpToggle.vue";
+import Pad from "@/components/Pad.vue";
 export default {
-    components:{ Pad },
-    methods:{
-        shouldBeHidden(i){
-            let noShows=['b','e']
-            return noShows.indexOf(this.$store.state.view.scale[i-1]) == -1 ? false : true;
-
-        }
+  components: { Pad, FlatSharpToggle },
+  methods: {
+    shouldBeHidden(i) {
+      let noShows = ["b", "e"];
+      return noShows.indexOf(this.$store.state.view.scale[i - 1]) == -1
+        ? false
+        : true;
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
